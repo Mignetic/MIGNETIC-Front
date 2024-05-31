a239ab8c83ea7063490d1f2569ed671f7eb7dc30
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-05-31 14:39:58 +0900

    애니메이션 추가 및 수정

--- src/components/homes/Secondary.js
+++ src/components/homes/Secondary.js
@@ -13,6 +13,8 @@ import bgImg from '../../images/secondary-bg.png'
 
 function Secondary() {
     const containerRef = useRef(null);
+    const iconRefs = useRef([]);
+
 
     useEffect(() => {
         const container = containerRef.current
@@ -31,32 +33,55 @@ function Secondary() {
         }
     }, [])
 
+    useEffect(() => {
+        const container = containerRef.current;
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('animate');
+                    }
+                });
+            },
+            { threshold: 0.1 }
+        );
+
+        iconRefs.current.forEach((icon) => {
+            observer.observe(icon);
+        });
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
+
     return (
         <div className="Secondary" ref={containerRef} style={{ backgroundImage: `url(${bgImg})` }}>
             <div className='iconsContainer'>
                 <div className='iconsContainer1'>
                     <div className='iconContainer1'>
                         <div className='iconContainer1-1'>
-                            <img src={icon1} className='icon1' />
+                            <img src={icon1} className='icon1' ref={(el) => (iconRefs.current.push(el))}/>
                             <div className='iconContainer1-2'>
-                                <img src={icon2} className='icon2' />
-                                <img src={icon3} className='icon3' />
+                                <img src={icon2} className='icon2' ref={(el) => (iconRefs.current.push(el))}/>
+                                <img src={icon3} className='icon3' ref={(el) => (iconRefs.current.push(el))}/>
                             </div>
                         </div>
                         <div className='iconContainer1-3'>
-                            <img src={icon4} className='icon4' />
-                            <img src={icon5} className='icon5' />
+                            <img src={icon4} className='icon4' ref={(el) => (iconRefs.current.push(el))}/>
+                            <img src={icon5} className='icon5' ref={(el) => (iconRefs.current.push(el))}/>
                         </div>
                     </div>
                     <div className='iconContainer2'>
-                        <img src={icon6} className='icon6' />
+                        <img src={icon6} className='icon6' ref={(el) => (iconRefs.current.push(el))}/>
                     </div>
                 </div>
 
                 <div className='iconsContainer2'>
-                    <img src={icon3} className='icon3-1' />
-                    <img src={icon7} className='icon7' />
-                    <p>학교 안에서<img src={icon8} className='icon8' /><br /> 나의 메이트 찾기!</p>
+                    <img src={icon3} className='icon3-1' ref={(el) => (iconRefs.current.push(el))}/>
+                    <img src={icon7} className='icon7' ref={(el) => (iconRefs.current.push(el))}/>
+                    <p>학교 안에서<img src={icon8} className='icon8' ref={(el) => (iconRefs.current.push(el))}/><br /> 나의 메이트 찾기!</p>
                 </div>
             </div>
         </div>
